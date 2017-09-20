94378e70190e906dbae7c16c6e7f3f31a40d3ff8
pacificsoftdevld <pacificsoftdev.ld@gmail.com>
2017-09-20 22:39:36 +0700

    update form login, register

--- ng-app/src/app/app-routing.module.ts
+++ ng-app/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { DashboardComponent }   from './dashboard/app.component';
 // auth
 import { LoginComponent }   from './auth/login/app.component';
 import { RegisterComponent }   from './auth/register/app.component';
+import { LoginRegisterComponent }   from './auth/login_register/app.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 	{ path: 'dashboard',  component: DashboardComponent },
 	{ path: 'login',  component: LoginComponent },
 	{ path: 'register',  component: RegisterComponent },
+	{ path: 'account',  component: LoginRegisterComponent },
 ];
 
 @NgModule({
--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -10,6 +12,7 @@ import { DashboardComponent } from './dashboard/app.component';
 // auth
 import { LoginComponent } from './auth/login/app.component';
 import { RegisterComponent } from './auth/register/app.component';
+import { LoginRegisterComponent } from './auth/login_register/app.component';
 
 @NgModule({
 	declarations: [
@@ -18,13 +21,18 @@ import { RegisterComponent } from './auth/register/app.component';
 		// auth
 		LoginComponent,
 		RegisterComponent,
+		LoginRegisterComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
 		HttpModule,
+		BrowserAnimationsModule,
+		MdButtonModule, 
+		MdCheckboxModule,
 	],
+	exports: [MdButtonModule, MdCheckboxModule],
 	providers: [],
 	bootstrap: [MainComponent]
 })
